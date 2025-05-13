f393428e5a665528c37aa5a3a3b212d99af85544
bhumesh2001 <bhumesh21@navgurukul.org>
2025-05-13 17:12:28 +0530

    new code

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const schoolRoutes = require('./routes/schoolRoutes');
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.status(200).json({ message: '🎉 Welcome to the School API! 🎓' });
+});
 app.use('/api', schoolRoutes);
 
 const PORT = process.env.PORT || 3000;
