ac05712967bf658090adc1b40bd64922c387f729
bhumesh2001 <bhumesh21@navgurukul.org>
2025-05-13 17:19:27 +0530

    new code

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => res.send(`<h1>🎉 Welcome to the School API! 🎓<h1/>`));
 app.use('/api', schoolRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
